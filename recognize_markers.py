abe3ec27432c5cea69c8609f5dc11c68fd1a449d
nicorobbo <nicolas.robson@epfl.ch>
2023-08-09 12:27:52 +0200

    Draws center point of recognized phone picture

--- recognize_markers.py
+++ recognize_markers.py
@@ -13,8 +13,21 @@ img = cv2.imread("phone.jpg")   #define image to analyse
 #Load Object Detector
 detector = HomogeneousBgDetector()
 
-contours = detector.detect_objects(img)
-print(contours)
+contours = detector.detect_objects(img)     #gives object boundary corners
+#print(contours)
+
+#Draw object boundaries
+for cnt in contours:
+    #Get a precise contour
+    cv2.polylines(img, [cnt], True, (255, 0, 0), 2)  #The polylines function will draw a polygon joining each "corner" point. "True" for closing the polygon, "2" for 2 pixel thick
+    #Get a rectangular contour
+    (x, y), (w, h), angle = cv2.minAreaRect(cnt)
+
+    cv2.circle(img, (int(x), int(y)), 5, (0, 0, 255), -1)   #Using int() so we don't have errors drawing when coordinates are not integers.
+    print(x, y)     #(x, y) Coordinates of the center of the object
+    print(w, h)
+    print(angle)
+
 
 #Show image
 cv2.imshow("Image",img)     #show image
