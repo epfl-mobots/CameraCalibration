dc744f312441dc1af08582dfa9883e5652b5d043
nicorobbo <nicolas.robson@epfl.ch>
2023-08-11 10:42:32 +0200

    Marker class impletmented->To logic

--- EXECUTABLE_Detect_Markers_ReadsIDs.py
+++ EXECUTABLE_Detect_Markers_ReadsIDs.py
@@ -16,6 +16,29 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 #Image Loading (if using a single image)
 #img = cv2.imread("phone_aruco_marker.jpg")   #define image to analyse
 
+class Marker:
+    def __init__(self, x, y, ID):
+        self.x = x
+        self.y = y
+        self.ID = ID
+        self.role = self.assign_role()
+
+    def assign_role(self):
+        if self.ID == 1:
+            return "TL"
+        elif self.ID == 2:
+            return "TR"
+        elif self.ID == 3:
+            return "BR"
+        elif self.ID == 4:
+            return "BL"
+        else:
+            return "Unknown"
+
+    def __repr__(self):
+        return f"Marker(ID: {self.ID}, Position: ({self.x}, {self.y}), Role: {self.role})"
+
+
 def read_markers():
     marker_list = []
     
@@ -31,21 +54,22 @@ def read_markers():
                 
                 # Draw green polygon around the marker
                 cv2.polylines(img, [int_corner], True, (0, 255, 0), 5)
-
+                
                 # Calculate the center of the marker
                 center = tuple(np.mean(int_corner[0], axis=0).astype(int))
-                
+
+                marker = Marker(center[0], center[1], ids[i][0])
+                marker_list.append(marker)
+
                 # Draw red circle at the center
                 cv2.circle(img, center, 5, (0, 0, 255), -1)
                 
                 # Display the marker ID
                 cv2.putText(img, "ID: " + str(ids[i][0]), (center[0] - 20, center[1] + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
                 
-                # Store center and ID in the list
-                marker_list.append((center, ids[i][0]))
 
         cv2.imshow("Image", img)
-        key = cv2.waitKey(1)  # take a new picture every 10,000ms
+        key = cv2.waitKey(10000)  # take a new picture every 10 000ms
         if key == 27:
             break
 
@@ -56,6 +80,8 @@ def read_markers():
 if __name__ == "__main__":
     marker_data = read_markers()
 
-    # Print center coordinates of markers and their IDs
-    for (x, y), marker_id in marker_data:
-        print(f"Marker ID: {marker_id}, Center: ({x}, {y})")
+    for marker in marker_data:
+        print(marker)
+        print(marker.ID)
+        print(marker.x)
+    
